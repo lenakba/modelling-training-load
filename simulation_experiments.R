ebf16bec4ab7c2fba0b95148fdef12402d162c4e
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2021-09-08 15:36:11 +0200

    made lists with cumulative effects for each relationship-function. did the same for simulating survival data (injuries) based on these cumulative effects. And the same for multiple Q matrices and crossbases. Finally, did sensitivty analysis for different arguments of the DLNM model for each relationship.

--- simulation_experiments.R
+++ simulation_experiments.R
@@ -34,6 +34,7 @@ lag_seq = lag_min:lag_max # number of days before current day assumed to affect 
 tl_predvalues = seq(min(tl_observed), max(tl_observed), 25)
 tl_predvalues_change = seq(min(tl_observed_change), max(tl_observed_change), 25)
 
+###################################Training load and lag structure functions###########################################
 # FUNCTIONS TO COMPUTE THE LOGIT AND INVERSE LOGIT TRANSFORMATIONS
 # logit = function(prob) log(prob/(1-prob))
 # invlogit = function(linpred) exp(linpred)/(1+exp(linpred))
@@ -53,10 +54,7 @@ wdecay = function(lag)exp(-lag/100)
 wexponential_decay = function(lag)exp(-lag/10)^2
 wdirection_flip = function(lag)case_when(lag <= 6 ~ exp(-lag/10)^2,
                                          lag >  6 ~ -exp(lag/50)^2)
-# list of functions
-flist = list(fj, fj, fj, flin)
-wlist = list(wconst, wdecay, wexponential_decay, wdirection_flip)
-wlist_change = list(wconst, wdecay, wexponential_decay)
+
 
 # COMBINATIONS OF FUNCTIONS USED TO SIMULATE DATA
 combsim = cbind(x_funs=rep(c("flin", "fj"), each=2),
@@ -119,6 +117,7 @@ persp(x = tl_predvalues_change, y = lag_seq, l_coefs_change[[3]], ticktype="deta
 dag0 = l_coefs_change[[1]][,1]
 plot(x = tl_predvalues_change, y = dag0)
 
+#################################################Simulating exposure histories###################################
 # simulate exposure histories
 # for nsub number of subjects
 # and t_max days under study
@@ -134,15 +133,7 @@ d_sim_tl_hist = l_tl_hist %>%
          day = rep(1:t_max, nsub),
          t_load_change = lead(t_load)-t_load) 
 
-# arrange in a matrix which will be used later
-d_sim_tl_hist_spread_day = 
-  d_sim_tl_hist  %>% select(-t_load_change) %>% 
-  pivot_wider(names_from = day, values_from = t_load) %>% select(-id) %>% as.matrix
-
-d_sim_tl_hist_spread_day_change = 
-  d_sim_tl_hist %>% select(-t_load) %>%
-  pivot_wider(names_from = day, values_from = t_load_change) %>% select(-id) %>% as.matrix
-
+#############################Calculate cumulative effect of training load given exposure histories#######################
 
 # function to compute the cumulative effect of training load exposure, requires:
 # tl_hist     the tl exposure history from t0 to max(t) for one participant
@@ -185,18 +176,25 @@ calc_cumeffs_all = function(l_sim_tl_hist, t_load_type = "amount", fvar, flag){
   d_cumeffs = unnest(l_sim_tl_hist, cols = c(data)) %>% rename(cumeff = data)
   d_cumeffs
 }
+# test: calc_cumeffs_all(l_load_nested, fj, wdecay)
+
+# lists of functions
+flist = list(fj, fj, fj, flin)
+wlist = list(wconst, wdecay, wexponential_decay, wdirection_flip)
+wlist_change = list(wconst, wdecay, wexponential_decay)
+
+# labels that allow the identification of the relationship in each element in a list
+names_rels = c("constant", "decay", "exponential_decay")
+name_extra = "flin_direction_flip"
 
 # nest simulated dataset on each individual
 l_load_nested = d_sim_tl_hist %>% nest(data = c(day, t_load, t_load_change)) 
-# test: calc_cumeffs_all(l_load_nested, fj, wdecay)
 
 # calc cumulative effects for each function in flist matched to each function in wlist
 l_cumeffs = map2(.x = flist,
                  .y = wlist, 
                  ~calc_cumeffs_all(l_load_nested, "amount", .x, .y))
-# add labels to identify relationship in each element in the list
-names_rels = c("constant", "decay", "exponential_decay")
-name_extra = "flin_direction_flip"
+# add labels and collapse to dataset
 l_cumeffs = map2(.x = l_cumeffs,
                  .y = c(names_rels, name_extra), 
                  ~.x %>% mutate(relationship = paste0(.y)))
@@ -219,11 +217,27 @@ l_load_nested = d_sim_tl_hist %>% nest(data = t_load)
 l_load_nested$data = l_load_nested$data %>% map(~calc_cumeff(.$t_load, flin, wdecay))
 d_load_cumeffs_lin_decay = unnest(l_load_nested, cols = c(data)) %>% rename(cumeff = data)
 
-# # permalgorithm requires a matrix of covariates
-# cumeff_j_decay = as.matrix(d_load_cumeffs_j_decay$cumeff)
-# cumeff_lin_decay = as.matrix(d_load_cumeffs_lin_decay$cumeff)
+########################################Simulate injuries based on cumulative effect of training load#####################
+l_cumeffs_mats = l_cumeffs %>% map(. %>% mutate(day = rep(1:t_max, nsub)) %>% 
+                    pivot_wider(names_from = id, values_from = cumeff) %>% 
+                    select(-day, -relationship) %>% as.matrix)
+
+l_cumeffs_mats_change = l_cumeffs_change %>% map(. %>% mutate(day = lead(rep(1:t_max, nsub))) %>% 
+                                                   filter(!is.na(cumeff)) %>% 
+                           pivot_wider(names_from = id, values_from = cumeff) %>% 
+                           select(-day, -relationship) %>% as.matrix)
+
+set.seed(1234)
+l_survival_sim = l_cumeffs_mats %>% map(., ~permalgorithm(nsub, t_max, Xmat = .,
+                                   censorRandom = runif(nsub, 1, t_max*2), betas=1))
+
+l_survival_sim_change = l_cumeffs_mats_change %>% map(., ~permalgorithm(nsub, t_max, Xmat = .,
+                                  censorRandom = runif(nsub, 1, (t_max)*2), betas=1))
+
 
 # arrange in a matrix which will be used later
+cumeff_j_decay = l_cumeffs[[2]]
+cumeff_lin_decay = l_cumeffs_change[[2]]
 cumeff_j_decay_i = 
   d_load_cumeffs_j_decay %>% mutate(day = rep(1:t_max, nsub)) %>% 
   pivot_wider(names_from = id, values_from = cumeff) %>% select(-day) %>% as.matrix
@@ -245,6 +259,157 @@ d_sim_surv_lin_decay = permalgorithm(nsub, t_max, Xmat = cumeff_lin_decay_i,
 
 d_sim_surv_j_decay %>% summarise(n_events = sum(Event == 1))
 d_sim_surv_lin_decay %>% summarise(n_events = sum(Event == 1))
+
+########################################Define the crossbasis for the DLNM method#####################
+
+# function to arrange the simulated survival data in counting process form
+counting_process_form = function(d_survival_sim){
+  d_follow_up_times = d_survival_sim %>% distinct(Id, Fup)
+  d_surv_lim = map2(.x = d_follow_up_times$Id,
+                    .y = d_follow_up_times$Fup,
+                    ~d_survival_sim %>% filter(Id == .x) %>% slice(.y)) %>% 
+                    bind_rows() %>% select(event = Event, exit = Stop, id = Id)
+  
+  # extracting timepoints in which an event happened
+  ftime = d_surv_lim %>% filter(event == 1) %>% distinct(exit) %>% arrange(exit) %>% pull()
+  
+  # arrange the survival data so that, for each individual, we have an interval of enter and exit times
+  # for each of the exit times above, with the information of whether or not they were injured at that time
+  # meaning we will have the same time intervals per participant
+  d_counting_process = survSplit(Surv(exit, event)~., d_surv_lim, cut = ftime, start="enter") %>% arrange(id)
+  d_counting_process
+}
+
+# function for calculating the q matrix given the survival data in counting process form
+# and the exposure history spread in wide format in a matrix
+calc_q_matrix = function(d_counting_process, d_tl_hist_wide){
+  # for each individual, for each of these exit times, we will extract the exposure history 
+  # for the given lag-time which we are interested in
+  # This is called the Q-matrix. The Q-matrix should be nrow(dataspl) X 0:lag_max dimensions.
+  q = map2(.x = d_counting_process$id, 
+            .y = d_counting_process$exit, 
+            ~exphist(d_tl_hist_wide[.x,], .y, c(lag_min, lag_max))) %>% 
+    do.call("rbind", .)
+  q
+  
+}
+
+# helper function to use both functions above in 1 step
+# resulting in a q-matrix
+from_sim_surv_to_q = function(d_survival_sim, d_tl_hist_wide){
+  d_counting_process = counting_process_form(d_survival_sim)
+  q = calc_q_matrix(d_counting_process, d_tl_hist_wide)
+  q
+}
+
+# arrange the exposure history in wide format in a matrix
+# which is neeeded for calculating the q-matrix for the crossbasis
+d_sim_tl_hist_spread_day = 
+  d_sim_tl_hist  %>% select(-t_load_change) %>% 
+  pivot_wider(names_from = day, values_from = t_load) %>% select(-id) %>% as.matrix
+
+d_sim_tl_hist_spread_day_change = 
+  d_sim_tl_hist %>% select(-t_load) %>%
+  pivot_wider(names_from = day, values_from = t_load_change) %>% select(-id) %>% as.matrix
+
+# list of q-matrices
+l_q_matrices = l_survival_sim %>% map(., ~from_sim_surv_to_q(., d_sim_tl_hist_spread_day))
+l_q_matrices_change = l_survival_sim_change %>% map(., ~from_sim_surv_to_q(., d_sim_tl_hist_spread_day_change))
+
+#-------------------------------------------Sensitivity analysis to determine best model
+# sensitivity analysis
+# what combination of lag and var functions
+# have the best AIC for each of the Q matrices?
+knots = 3:6
+arglist_splines = knots %>% map(., ~list(fun="ns", knots = .))
+arglist_poly = list(list(fun="poly",degree=2))
+arglist_lin = list(list(fun = "lin"))
+
+arglist1 = append(arglist_splines, arglist_poly)
+arglist = append(arglist1, arglist_lin)
+
+# instead of making a list of lists, copy-paste
+# easier to keep head straight on permutations
+# this needs careful coding
+crossbasis_j_constant = arglist %>% 
+  map(., ~crossbasis(l_q_matrices[[1]],
+                     lag=c(lag_min, lag_max),
+                     argvar = list(fun="ns", knots = 3, intercept = FALSE),
+                     arglag = .))
+
+crossbasis_j_decay = arglist %>% 
+  map(., ~crossbasis(l_q_matrices[[2]],
+                     lag=c(lag_min, lag_max),
+                     argvar = list(fun="ns", knots = 3, intercept = FALSE),
+                     arglag = .))
+
+crossbasis_j_exponential_decay = arglist %>% 
+  map(., ~crossbasis(l_q_matrices[[3]],
+                     lag=c(lag_min, lag_max),
+                     argvar = list(fun="ns", knots = 3, intercept = FALSE),
+                     arglag = .))
+
+crossbasis_flin_direction_flip = arglist %>% 
+  map(., ~crossbasis(l_q_matrices[[4]],
+                     lag=c(lag_min, lag_max),
+                     argvar = list(fun="lin", intercept = FALSE),
+                     arglag = .))
+
+# need data on counting process form
+l_counting_survival_sim = l_survival_sim %>% map(., ~counting_process_form(.))
+
+names_dlnm_funs = c("splines_3", "splines_4", "splines_5", "splines_6", "ploy2", "lin")
+names_truerel = c(names_rels, name_extra)
+d_aic = tibble(
+  aic = numeric(),
+  fun = character(),
+  rel = character()
+  )
+
+aic_vec = c()
+for(i in 1:length(arglist)){
+  surfit_j_constant = coxph(Surv(enter, exit, event) ~ crossbasis_j_constant[[i]], l_counting_survival_sim[[1]], y = FALSE, ties = "efron")
+  surfit_j_decay = coxph(Surv(enter, exit, event) ~ crossbasis_j_decay[[i]], l_counting_survival_sim[[2]], y = FALSE, ties = "efron")
+  survfit_j_exponential_decay = coxph(Surv(enter, exit, event) ~ crossbasis_j_exponential_decay[[i]], l_counting_survival_sim[[3]], y = FALSE, ties = "efron")
+  survfit_lin_direction_flip = coxph(Surv(enter, exit, event) ~ crossbasis_flin_direction_flip[[i]], l_counting_survival_sim[[4]], y = FALSE, ties = "efron")
+  aic = c(AIC(surfit_j_constant), AIC(surfit_j_decay), AIC(survfit_j_exponential_decay), AIC(survfit_lin_direction_flip))
+  aic_vec = append(aic_vec, aic)
+}
+
+d_aic_amount_load = enframe(aic_vec, name = NULL) %>% 
+                    mutate(fun_names = rep(names_dlnm_funs, each = 4), 
+                           true_rels = rep(names_truerel, length(arglist))) %>% rename(aic = value)
+best_aics_tl_amount = d_aic_amount_load %>% group_by(true_rels) %>% filter(aic == min(aic))
+
+
+
+
+
+
+# RUN THE MODEL, SAVING IT IN THE LIST WITH MINIMAL INFO (SAVE MEMORY)
+modellist = list()
+modellist[[1]] = coxph(Surv(enter, exit, event) ~ cb_j_decay, dataspl_j_decay, y = FALSE, ties = "efron")
+modellist[[2]] = coxph(Surv(enter, exit, event) ~ cb_lin_decay, dataspl_lin_decay, y = FALSE, ties = "efron")
+mod_j_decay = modellist[[1]]
+mod_lin_decay = modellist[[2]]
+AIC(mod_j_decay)
+AIC(mod_lin_decay)
+
+
+
+
+
+# DEFINE THE CROSS-BASIS
+# you can make a list of functions for argvar and arglag, with splines etc.
+cb_j_decay = crossbasis(q_j_decay,
+                        lag=c(lag_min, lag_max),
+                        argvar = list(fun="ns", knots = 3, intercept = FALSE),
+                        arglag = list(fun="lin", intercept=TRUE))
+
+cb_lin_decay = crossbasis(q_lin_decay, 
+                          lag=c(lag_min, lag_max), 
+                          argvar = list("lin", intercept = FALSE), 
+                          arglag = list(fun="lin", intercept = TRUE))
 
 
 #------j decay
